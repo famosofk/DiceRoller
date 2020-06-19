7fd505743246a325d9596d56026ae5f816d7fff3
FamosoFK <fabianogmjunior@gmail.com>
2020-06-19 10:09:47 -0300

    Finished.
    Signed-off-by: FamosoFK <Fabianogmjunior@gmail.com>

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+private lateinit var imageView: ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        imageView = findViewById(R.id.imageView)
+
+
+        rollButton.text = "Dice roll"
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultado = Random.nextInt(6) + 1
+        val drawableResource = when (resultado) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        imageView.setImageResource(drawableResource)
+
     }
 }
